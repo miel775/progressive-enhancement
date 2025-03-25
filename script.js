d9d9337ec4897f8e93522d33dfcbe9e02353617f
miel775 <miel.van.sleeuwen.van.sleeuwen@hva.nl>
2025-03-25 13:37:13 +0100

    HTML opbouw
    HTML opbouw
    
    #3

---
+++ script.js
@@ -0,0 +1,14 @@
+const audio = document.getElementById('audioFile');
+
+function toggleControls() {
+    if (audio.hasAttribute('controls')) {
+        audio.removeAttribute('controls');
+    }
+    else {
+        audio.setAttribute('controls','controls');
+    }
+}
+
+function openNav() {
+    document.getElementById('')
+}
